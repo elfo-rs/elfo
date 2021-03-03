1b6f126f609a1a6a0c01e8fd170586c636652e0b
Paul Loyd <pavelko95@gmail.com>
2021-03-03 18:34:04 +0300

    refactor(core): add `ward!`

--- elfo-core/src/lib.rs
+++ elfo-core/src/lib.rs
@@ -7,6 +7,15 @@ pub use crate::{
     envelope::{Envelope, Message, ReplyToken, Request},
 };
 
+/// Returns the contents of a `Option<T>`'s `Some(T)`, otherwise it returns
+/// early from the function. Can alternatively have an `else` branch, or an
+/// alternative "early return" statement, like `break` or `continue` for loops.
+macro_rules! ward {
+    ($o:expr) => (ward!($o, else { return; }));
+    ($o:expr, else $body:block) => { if let Some(x) = $o { x } else { $body }; };
+    ($o:expr, $early:stmt) => (ward!($o, else { $early }));
+}
+
 pub mod trace_id;
 
 pub mod errors {
