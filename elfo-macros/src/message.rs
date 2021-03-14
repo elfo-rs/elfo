c4a61a43f34c6b65351c2b8944ae41979e19f4e9
Paul Loyd <pavelko95@gmail.com>
2021-03-14 18:00:51 +0300

    feat(macros): add the "crate" property

--- elfo-macros/src/message.rs
+++ elfo-macros/src/message.rs
@@ -3,31 +3,54 @@ use std::time::UNIX_EPOCH;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
+    parenthesized,
     parse::{Error as ParseError, Parse, ParseStream},
-    parse_macro_input, DeriveInput, Ident, Path, Token,
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    DeriveInput, Ident, Path, Token, Type,
 };
 
+#[derive(Debug)]
 struct MessageArgs {
-    response: Option<Path>,
+    ret: Option<Type>,
+    crate_: Path,
 }
 
 impl Parse for MessageArgs {
     fn parse(input: ParseStream<'_>) -> Result<Self, ParseError> {
+        // TODO: support any order of attributes.
+
+        let mut args = MessageArgs {
+            ret: None,
+            crate_: parse_quote!(::elfo),
+        };
+
         // `#[message]`
-        if input.is_empty() {
-            return Ok(MessageArgs { response: None });
+        // `#[message(ret(A))]`
+        // `#[message(ret(A), crate = "some")]`
+        // `#[message(crate = some::path)]`
+        while !input.is_empty() {
+            let ident: Ident = input.parse()?;
+
+            match ident.to_string().as_str() {
+                "ret" => {
+                    let _: Token![=] = input.parse()?;
+                    args.ret = Some(input.parse()?);
+                }
+                // TODO: call it `crate` like in linkme?
+                "elfo" => {
+                    let _: Token![=] = input.parse()?;
+                    args.crate_ = input.parse()?;
+                }
+                attr => panic!("invalid attribute: {}", attr),
+            }
+
+            if !input.is_empty() {
+                let _: Token![,] = input.parse()?;
+            }
         }
 
-        // `#[message(ret = Ret)]`
-        let ident: Ident = input.parse()?;
-        assert_eq!(ident.to_string(), "ret");
-
-        let _: Token![=] = input.parse()?;
-        let path: Path = input.parse()?;
-
-        Ok(MessageArgs {
-            response: Some(path),
-        })
+        Ok(args)
     }
 }
 
@@ -39,16 +62,18 @@ fn gen_ltid() -> u32 {
 
 pub fn message_impl(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as MessageArgs);
+
     // TODO: what about parsing into something cheaper?
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident.clone();
     let mod_name = Ident::new(&format!("_elfo_{}", name), name.span());
     let ltid = gen_ltid();
+    let crate_ = args.crate_;
 
-    let derive_request = if let Some(response) = args.response {
+    let derive_request = if let Some(ret) = args.ret {
         quote! {
-            impl elfo::Request for #name {
-                type Response = #response;
+            impl #crate_::Request for #name {
+                type Response = #ret;
             }
         }
     } else {
@@ -60,22 +85,22 @@ pub fn message_impl(args: TokenStream, input: TokenStream) -> TokenStream {
         #[derive(Clone)]
         #input
 
-        impl ::elfo::Message for #name {
-            const _LTID: elfo::_priv::LocalTypeId = #ltid;
+        impl #crate_::Message for #name {
+            const _LTID: #crate_::_priv::LocalTypeId = #ltid;
         }
 
         #[allow(non_snake_case)]
         mod #mod_name {
             use super::#name;
 
-            use ::elfo::_priv::{MESSAGE_LIST, MessageVTable, smallbox::{smallbox}, AnyMessage, linkme};
+            use #crate_::_priv::{MESSAGE_LIST, MessageVTable, smallbox::{smallbox}, AnyMessage, linkme};
 
             fn clone(message: &AnyMessage) -> AnyMessage {
                 smallbox!(message.downcast_ref::<#name>().expect("invalid vtable").clone())
             }
 
             #[linkme::distributed_slice(MESSAGE_LIST)]
-            #[linkme(crate = elfo::_priv::linkme)]
+            #[linkme(crate = #crate_::_priv::linkme)]
             static VTABLE: MessageVTable = MessageVTable {
                 ltid: #ltid,
                 clone,
