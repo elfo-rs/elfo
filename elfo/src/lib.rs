7beedeb87ec33f5e8582adeda6818f4b4d1198eb
Paul Loyd <pavelko95@gmail.com>
2021-03-26 11:40:40 +0300

    feat!(prelude): remove Addr

--- elfo/src/lib.rs
+++ elfo/src/lib.rs
@@ -4,5 +4,5 @@ pub use elfo_core::*;
 pub use elfo_macros::{message, msg};
 
 pub mod prelude {
-    pub use super::{message, msg, ActorGroup, Addr, Context, Schema};
+    pub use super::{message, msg, ActorGroup, Context, Schema};
 }
