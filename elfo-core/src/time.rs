a9c0c6bc518bbda07e6994062c499f9622e708eb
Paul Loyd <pavelko95@gmail.com>
2021-04-02 18:48:55 +0300

    feat(core/time): add `Interval::after()`

--- elfo-core/src/time.rs
+++ elfo-core/src/time.rs
@@ -16,11 +16,12 @@ use crate::{
 
 pub struct Interval<F> {
     message_factory: F,
-    state: Mutex<Option<State>>,
+    state: Mutex<State>,
 }
 
 struct State {
     sleep: Pin<Box<Sleep>>,
+    start_at: Option<Instant>,
     period: Duration,
 }
 
@@ -28,44 +29,46 @@ impl<F> Interval<F> {
     pub fn new(f: F) -> Self {
         Self {
             message_factory: f,
-            state: Mutex::new(None),
+            state: Mutex::new(State {
+                sleep: Box::pin(time::sleep_until(Instant::now())),
+                start_at: None,
+                period: Duration::new(0, 0),
+            }),
         }
     }
 
-    #[inline]
-    pub fn set_period(&self, period: Duration) {
+    pub fn after(self, after: Duration) -> Self {
+        let when = Instant::now() + after;
+        let mut state = self.state.lock();
+        state.start_at = Some(when);
+        state.sleep.as_mut().reset(when);
+        drop(state);
+        self
+    }
+
+    pub fn set_period(&self, new_period: Duration) {
+        assert!(new_period > Duration::new(0, 0));
+
         let mut state = self.state.lock();
 
-        if let Some(state) = state.as_mut() {
-            if period != state.period {
-                state.do_set_period(period);
-            }
-        } else {
-            let sleep = time::sleep(period);
-            *state = Some(State {
-                sleep: Box::pin(sleep),
-                period,
-            });
+        if new_period == state.period {
+            return;
+        }
+
+        let old_period = state.period;
+        state.period = new_period;
+
+        if state.start_at.is_none() {
+            let new_deadline = state.sleep.deadline() - old_period + new_period;
+            state.sleep.as_mut().reset(new_deadline);
         }
     }
 
     pub fn reset(&self) {
         let mut state = self.state.lock();
-
-        if let Some(state) = state.as_mut() {
-            let new_deadline = Instant::now() + state.period;
-            state.sleep.as_mut().reset(new_deadline);
-        }
-    }
-}
-
-impl State {
-    #[cold]
-    fn do_set_period(&mut self, period: Duration) {
-        let sleep = self.sleep.as_mut();
-        let new_deadline = sleep.deadline() - self.period + period;
-        self.period = period;
-        sleep.reset(new_deadline);
+        let new_deadline = Instant::now() + state.period;
+        state.start_at = None;
+        state.sleep.as_mut().reset(new_deadline);
     }
 }
 
@@ -77,17 +80,25 @@ where
     fn poll_recv(&self, cx: &mut task::Context<'_>) -> Poll<Option<Envelope>> {
         let mut state = self.state.lock();
 
-        if let Some(state) = state.as_mut() {
-            if state.sleep.as_mut().poll(cx).is_ready() {
-                let sleep = state.sleep.as_mut();
-                let new_deadline = sleep.deadline() + state.period;
-                sleep.reset(new_deadline);
-
-                let message = (self.message_factory)();
-                let kind = MessageKind::Regular { sender: Addr::NULL };
-                let envelope = Envelope::new(message, kind).upcast();
-                return Poll::Ready(Some(envelope));
+        if state.sleep.as_mut().poll(cx).is_ready() {
+            // It hasn't been configured, so just ignore it.
+            if state.period == Duration::new(0, 0) && state.start_at.is_none() {
+                return Poll::Pending;
             }
+
+            state.start_at = None;
+
+            // Now reset the underlying timer.
+            let period = state.period;
+            let sleep = state.sleep.as_mut();
+            let new_deadline = sleep.deadline() + period;
+            sleep.reset(new_deadline);
+
+            // Emit a message.
+            let message = (self.message_factory)();
+            let kind = MessageKind::Regular { sender: Addr::NULL };
+            let envelope = Envelope::new(message, kind).upcast();
+            return Poll::Ready(Some(envelope));
         }
 
         Poll::Pending
