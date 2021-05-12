6e35b7094e9e819657491c1150d66820156ebfdc
Paul Loyd <pavelko95@gmail.com>
2021-05-12 18:32:57 +0300

    test(streams): check `set()` and `replace()`

--- elfo/tests/streams.rs
+++ elfo/tests/streams.rs
@@ -7,29 +7,50 @@ use elfo::{config::AnyConfig, prelude::*, stream};
 struct SomeMessage(u32);
 
 #[message]
+struct Set(Vec<u32>);
+
+#[message]
+struct Replace(Vec<u32>);
+
+#[message]
 #[derive(PartialEq)]
 struct EndOfMessages;
 
-fn samples() -> Schema {
-    ActorGroup::new().exec(|ctx| async move {
-        let stream1 =
-            stream::Stream::new(futures::stream::iter(vec![SomeMessage(0), SomeMessage(1)]));
+#[tokio::test]
+async fn it_handles_basic_operations() {
+    let group = ActorGroup::new().exec(|ctx| async move {
+        let stream = stream::Stream::new(futures::stream::iter(vec![SomeMessage(0)]));
 
-        let mut ctx = ctx.with(stream1);
+        let mut ctx = ctx.with(&stream);
 
         while let Some(envelope) = ctx.recv().await {
             msg!(match envelope {
                 m @ SomeMessage(_) => ctx.send(m).await.unwrap(),
+                Set(d) => stream.set(futures::stream::iter(
+                    d.into_iter().map(SomeMessage).collect::<Vec<_>>()
+                )),
+                Replace(d) => {
+                    let _ = stream.replace(futures::stream::iter(
+                        d.into_iter().map(SomeMessage).collect::<Vec<_>>(),
+                    ));
+                }
                 EndOfMessages => ctx.send(EndOfMessages).await.unwrap(),
             })
         }
-    })
-}
+    });
 
-#[tokio::test]
-async fn it_works() {
-    let mut proxy = elfo::test::proxy(samples(), AnyConfig::default()).await;
-
+    let mut proxy = elfo::test::proxy(group, AnyConfig::default()).await;
     assert_msg_eq!(proxy.recv().await, SomeMessage(0));
-    assert_msg_eq!(proxy.recv().await, SomeMessage(1));
+    assert!(proxy.try_recv().is_none());
+
+    proxy.send(Set((1..5).collect())).await;
+    for i in 1..5 {
+        assert_msg_eq!(proxy.recv().await, SomeMessage(i));
+    }
+    assert!(proxy.try_recv().is_none());
+
+    proxy.send(Replace((6..8).collect())).await;
+    for i in 6..8 {
+        assert_msg_eq!(proxy.recv().await, SomeMessage(i));
+    }
 }
