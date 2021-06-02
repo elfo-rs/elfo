dd6c056be9f952a6d98fd08ed36a2c4253e8ff6a
Paul Loyd <pavelko95@gmail.com>
2021-06-02 08:35:52 +0300

    test: add more tests of restarting

--- elfo/tests/restarting.rs
+++ elfo/tests/restarting.rs
@@ -1,5 +1,9 @@
 #![cfg(feature = "test-util")]
 
+use std::{panic::AssertUnwindSafe, time::Duration};
+
+use futures::FutureExt;
+
 use elfo::prelude::*;
 
 #[message]
@@ -25,5 +29,32 @@ async fn it_restarts_explicitly() {
     for _ in 1..5 {
         proxy.send(Terminate).await;
         assert_msg!(proxy.recv().await, Terminated);
+    }
+}
+
+#[tokio::test]
+async fn it_restarts_with_timeout_after_failures() {
+    tokio::time::pause();
+
+    let schema = ActorGroup::new().exec(move |mut ctx| async move {
+        while let Some(envelope) = ctx.recv().await {
+            msg!(match envelope {
+                Terminate { .. } => panic!("boom!"),
+                _ => unreachable!(),
+            });
+        }
+    });
+
+    let mut proxy = elfo::test::proxy(schema, elfo::config::AnyConfig::default()).await;
+
+    for _ in 1..5 {
+        proxy.send(Terminate).await;
+
+        let r = AssertUnwindSafe(async { proxy.recv().await })
+            .catch_unwind()
+            .await;
+        assert!(r.is_err());
+
+        tokio::time::advance(Duration::from_secs(5)).await;
     }
 }
