d811cb8f3126398e639ea398f04d646482a47f3e
Paul Loyd <pavelko95@gmail.com>
2021-10-20 21:44:00 +0300

    test: make deterministic

--- elfo/tests/subscription_to_statuses.rs
+++ elfo/tests/subscription_to_statuses.rs
@@ -7,7 +7,7 @@ use elfo::{
     prelude::*,
     routers::{MapRouter, Outcome},
     test::Proxy,
-    ActorStatus, ActorStatusKind, Envelope,
+    ActorStatus, ActorStatusKind,
 };
 
 #[message]
@@ -48,16 +48,25 @@ async fn run_group() -> Proxy {
 }
 
 #[track_caller]
-fn check(envelope: &Envelope, key: u32, kind: ActorStatusKind, details: Option<&str>) {
-    msg!(match envelope {
-        ActorStatusReport { meta, status, .. } => {
-            assert_eq!(meta.group, "subject");
-            assert_eq!(meta.key, key.to_string());
-            assert_eq!(status.kind(), kind);
-            assert_eq!(status.details(), details);
-        }
-        _ => unreachable!(),
-    })
+fn check_seq(proxy: &mut Proxy, expected: &[(u32, ActorStatusKind, Option<&str>)]) {
+    let mut actual = Vec::new();
+    while let Some(envelope) = proxy.try_recv() {
+        msg!(match envelope {
+            ActorStatusReport { meta, status, .. } => actual.push((meta, status)),
+            _ => unreachable!(),
+        })
+    }
+
+    // Sort messages to make tests deterministic.
+    actual.sort_by_key(|(meta, _)| meta.key.clone());
+    assert_eq!(actual.len(), expected.len());
+
+    for (a, e) in actual.iter().zip(expected) {
+        assert_eq!(a.0.group, "subject");
+        assert_eq!(a.0.key, e.0.to_string());
+        assert_eq!(a.1.kind(), e.1);
+        assert_eq!(a.1.details(), e.2);
+    }
 }
 
 #[tokio::test]
@@ -82,58 +91,94 @@ async fn it_works() {
 
     // Now subscribe to status reports.
     proxy.send(SubscribeToActorStatuses::default()).await;
+    proxy.sync().await;
 
     // Firstly, we should receive statuses of unstopped actors.
-    check(&proxy.recv().await, 3, Failed, Some("panic: oops"));
-    check(&proxy.recv().await, 1, Normal, Some("on Start"));
-    check(&proxy.recv().await, 4, Normal, Some("on Start"));
-    // There is no status for `2` because it's terminated.
+    check_seq(
+        &mut proxy,
+        &[
+            (1, Normal, Some("on Start")),
+            // There is no status for `2` because it's terminated.
+            (3, Failed, Some("panic: oops")),
+            (4, Normal, Some("on Start")),
+        ],
+    );
 
     // Start another one.
     proxy.send(Start(5)).await;
-    check(&proxy.recv().await, 5, Initializing, None);
-    check(&proxy.recv().await, 5, Normal, None);
-    check(&proxy.recv().await, 5, Normal, Some("on Start"));
+    proxy.sync().await;
+
+    check_seq(
+        &mut proxy,
+        &[
+            (5, Initializing, None),
+            (5, Normal, None),
+            (5, Normal, Some("on Start")),
+        ],
+    );
 
     // Fail some.
     proxy.send(Fail(1)).await;
-    check(&proxy.recv().await, 1, Normal, Some("on Fail"));
-    check(&proxy.recv().await, 1, Failed, Some("panic: oops"));
+    proxy.sync().await;
+
+    check_seq(
+        &mut proxy,
+        &[
+            (1, Normal, Some("on Fail")),
+            (1, Failed, Some("panic: oops")),
+        ],
+    );
 
     // Test another subscription.
     let mut subproxy = proxy.subproxy().await;
     subproxy.send(SubscribeToActorStatuses::default()).await;
-    check(&subproxy.recv().await, 3, Failed, Some("panic: oops"));
-    check(&subproxy.recv().await, 5, Normal, Some("on Start"));
-    check(&subproxy.recv().await, 1, Failed, Some("panic: oops"));
-    check(&subproxy.recv().await, 4, Normal, Some("on Start"));
+    subproxy.sync().await;
+
+    check_seq(
+        &mut subproxy,
+        &[
+            (1, Failed, Some("panic: oops")),
+            (3, Failed, Some("panic: oops")),
+            (4, Normal, Some("on Start")),
+            (5, Normal, Some("on Start")),
+        ],
+    );
 
     // Stop some.
     proxy.send(Stop(4)).await;
-    check(&proxy.recv().await, 4, Normal, Some("on Stop"));
-    check(&proxy.recv().await, 4, Terminated, None);
-    check(&subproxy.recv().await, 4, Normal, Some("on Stop"));
-    check(&subproxy.recv().await, 4, Terminated, None);
-
     proxy.sync().await;
-    assert!(proxy.try_recv().is_none());
     subproxy.sync().await;
-    assert!(subproxy.try_recv().is_none());
+
+    check_seq(
+        &mut proxy,
+        &[(4, Normal, Some("on Stop")), (4, Terminated, None)],
+    );
+    check_seq(
+        &mut subproxy,
+        &[(4, Normal, Some("on Stop")), (4, Terminated, None)],
+    );
 
     // Wait for restarting.
     tokio::time::sleep(Duration::from_secs(100)).await;
-
-    check(&proxy.recv().await, 3, Initializing, None);
-    check(&proxy.recv().await, 3, Normal, None);
-    check(&proxy.recv().await, 1, Initializing, None);
-    check(&proxy.recv().await, 1, Normal, None);
-    check(&subproxy.recv().await, 3, Initializing, None);
-    check(&subproxy.recv().await, 3, Normal, None);
-    check(&subproxy.recv().await, 1, Initializing, None);
-    check(&subproxy.recv().await, 1, Normal, None);
-
     proxy.sync().await;
-    assert!(proxy.try_recv().is_none());
     subproxy.sync().await;
-    assert!(subproxy.try_recv().is_none());
+
+    check_seq(
+        &mut proxy,
+        &[
+            (1, Initializing, None),
+            (1, Normal, None),
+            (3, Initializing, None),
+            (3, Normal, None),
+        ],
+    );
+    check_seq(
+        &mut subproxy,
+        &[
+            (1, Initializing, None),
+            (1, Normal, None),
+            (3, Initializing, None),
+            (3, Normal, None),
+        ],
+    );
 }
