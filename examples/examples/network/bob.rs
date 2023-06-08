891d6ea14b9cb089bb4f845ec325cfd1eacd1066
Paul Loyd <pavelko95@gmail.com>
2023-06-08 18:21:24 +0400

    chore(examples/network): update an example

---
+++ examples/examples/network/bob.rs
@@ -0,0 +1,54 @@
+use elfo::prelude::*;
+use tracing::{info, warn};
+
+use crate::common::Hello;
+
+fn consumer() -> Blueprint {
+    ActorGroup::new().exec(|mut ctx| async move {
+        while let Some(envelope) = ctx.recv().await {
+            let sender = envelope.sender();
+
+            msg!(match envelope {
+                Hello(i) => {
+                    info!("received Hello({})", i);
+
+                    if let Err(err) = ctx.send_to(sender, Hello(i)).await {
+                        warn!("cannot say Hello({}) back: {}", i, err);
+                    }
+                }
+            });
+        }
+    })
+}
+
+pub(crate) fn topology() -> elfo::Topology {
+    elfo::_priv::node::set_node_no(2);
+
+    let topology = elfo::Topology::empty();
+    let logger = elfo::batteries::logger::init();
+    let telemeter = elfo::batteries::telemeter::init();
+
+    // System groups.
+    let loggers = topology.local("system.loggers");
+    let telemeters = topology.local("system.telemeters");
+    let dumpers = topology.local("system.dumpers");
+    let configurers = topology.local("system.configurers").entrypoint();
+    let network = topology.local("system.network");
+
+    // Local user groups.
+    let consumers = topology.local("consumers");
+
+    loggers.mount(logger);
+    telemeters.mount(telemeter);
+    dumpers.mount(elfo::batteries::dumper::new());
+    network.mount(elfo::batteries::network::new(&topology));
+    consumers.mount(consumer());
+
+    let config_path = "examples/examples/network/bob.toml";
+    configurers.mount(elfo::batteries::configurer::from_path(
+        &topology,
+        config_path,
+    ));
+
+    topology
+}
--- examples/examples/network/main.rs
+++ examples/examples/network/main.rs
@@ -1,11 +1,12 @@
-mod backend;
-mod core;
+mod alice;
+mod bob;
+mod common;
 
 #[tokio::main]
 async fn main() {
     let topology = match std::env::args().nth(1).unwrap().as_str() {
-        "core" => core::topology(),
-        "backend" => backend::topology(),
+        "alice" => alice::topology(),
+        "bob" => bob::topology(),
         _ => unreachable!(),
     };
 
