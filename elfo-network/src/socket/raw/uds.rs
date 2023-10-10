e40f6370e344f715aa625e0dd4ccdeb973d841a6
Paul Loyd <pavelko95@gmail.com>
2023-10-10 17:14:55 +0400

    feat(network/uds): remove the socket file on bind and drop

--- elfo-network/src/socket/raw/uds.rs
+++ elfo-network/src/socket/raw/uds.rs
@@ -2,13 +2,14 @@
 
 use std::{
     fmt,
+    io::Result as IoResult,
     path::{Path, PathBuf},
 };
 
 use eyre::Result;
 use futures::Stream;
 use tokio::net::{unix::pid_t as Pid, UnixListener, UnixStream};
-use tracing::warn;
+use tracing::{debug, warn};
 
 pub(super) use tokio::net::unix::{OwnedReadHalf, OwnedWriteHalf};
 
@@ -50,12 +51,12 @@ pub(super) async fn connect(addr: &Path) -> Result<Socket> {
 }
 
 pub(super) fn listen(addr: &Path) -> Result<impl Stream<Item = Socket> + 'static> {
-    let listener = UnixListener::bind(addr)?;
+    let listener = Listener::bind(addr)?;
 
-    let accept = move |(listener, addr): (UnixListener, PathBuf)| async move {
+    let accept = move |(listener, addr): (Listener, PathBuf)| async move {
         loop {
             match listener.accept().await {
-                Ok((stream, _)) => {
+                Ok(stream) => {
                     let socket = prepare_stream(stream, &addr);
                     return Some((socket, (listener, addr)));
                 }
@@ -73,4 +74,46 @@ pub(super) fn listen(addr: &Path) -> Result<impl Stream<Item = Socket> + 'static
     };
 
     Ok(futures::stream::unfold((listener, addr.to_owned()), accept))
+}
+
+/// A wrapper around `UnixListener` which unlinks the socket file during binding
+/// and destruction. It requires because the kernel doesn't do it automatically.
+///
+/// Deletion before binding is controversial because it stops the previous owner
+/// of the socket to accept new connections, that can happen in the case of
+/// misconfiguration. However, it allows to recover after a crash (or SIGKILL)
+/// of the node where destructors aren't called.
+struct Listener {
+    path: PathBuf,
+    inner: UnixListener,
+}
+
+impl Listener {
+    fn bind(path: &Path) -> IoResult<Self> {
+        remove_file_with_log(path);
+        let path = path.to_owned();
+        UnixListener::bind(&path).map(|inner| Listener { path, inner })
+    }
+
+    async fn accept(&self) -> IoResult<UnixStream> {
+        self.inner.accept().await.map(|(socket, _)| socket)
+    }
+}
+
+impl Drop for Listener {
+    fn drop(&mut self) {
+        remove_file_with_log(&self.path);
+    }
+}
+
+fn remove_file_with_log(path: &Path) {
+    match std::fs::remove_file(path) {
+        Ok(()) => debug!(message = "removed UDS socket", path = %path.display()),
+        Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
+        Err(err) => warn!(
+            message = "cannot remove UDS socket",
+            error = %err,
+            path = %path.display(),
+        ),
+    }
 }
