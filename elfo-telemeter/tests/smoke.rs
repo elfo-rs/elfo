eaa4c31e2f659fdda84b1ee5832049295b30f190
Paul Loyd <pavelko95@gmail.com>
2025-07-10 09:47:01 +0400

    test(telemeter): check more corner cases

--- elfo-telemeter/tests/smoke.rs
+++ elfo-telemeter/tests/smoke.rs
@@ -1,9 +1,10 @@
 //! A smoke integration test for the OpenMetrics telemeter.
 
+use eyre::Result;
 use toml::toml;
 
 #[tokio::test]
-async fn it_works() {
+async fn it_works() -> Result<()> {
     let config = toml! {
         sink = "OpenMetrics"
         listen = "127.0.0.1:9042"
@@ -12,19 +13,21 @@ async fn it_works() {
     let blueprint = elfo_telemeter::init();
     let _proxy = elfo_test::proxy(blueprint, config).await;
 
-    let content = reqwest::get("http://127.0.0.1:9042/metrics")
-        .await
-        .unwrap()
+    // Get metrics with GZIP compression
+
+    let client = reqwest::Client::builder().gzip(true).build()?;
+    let content = client
+        .get("http://127.0.0.1:9042/metrics")
+        .send()
+        .await?
         .text()
-        .await
-        .unwrap();
+        .await?;
 
     let some_expected_parts = [
-        r#"elfo_actor_status_changes_total{actor_group="system.init",status="Initializing"} 0"#,
-        r#"elfo_active_actors{actor_group="subject",status="Normal"} 1"#,
-        r#"elfo_sent_messages_total{actor_group="subject",message="Render",protocol="elfo-telemeter"} 0"#,
+        r#"elfo_actor_status_changes_total{actor_group="system.init",status="Initializing"}"#,
+        r#"elfo_active_actors{actor_group="subject",status="Normal"}"#,
+        r#"elfo_sent_messages_total{actor_group="subject",message="Render",protocol="elfo-telemeter"}"#,
         r#"elfo_busy_time_seconds{actor_group="subject",quantile="0.75"}"#,
-        r#"elfo_message_handling_time_seconds{actor_group="system.configurers",message="<Startup>",quantile="0.75"}"#,
         r#"elfo_message_waiting_time_seconds_min{actor_group="subject"}"#,
         "# TYPE elfo_sent_messages_total counter",
         "# TYPE elfo_active_actors gauge",
@@ -32,7 +35,38 @@ async fn it_works() {
         "EOF",
     ];
 
+    println!("Metrics content (first request):\n{content}");
+
     for part in some_expected_parts {
         assert!(content.contains(part), "not found: {part}");
     }
+
+    // Get metrics without compression
+
+    let content = reqwest::Client::builder()
+        .gzip(false)
+        .build()?
+        .get("http://127.0.0.1:9042/metrics")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("Metrics content (second request):\n{content}");
+
+    for part in some_expected_parts {
+        assert!(content.contains(part), "not found: {part}");
+    }
+
+    // Not supported methods
+
+    let response = client.post("http://127.0.0.1:9042/metrics").send().await?;
+    assert_eq!(response.status(), reqwest::StatusCode::METHOD_NOT_ALLOWED);
+
+    // Not supported endpoints
+
+    let response = client.get("http://127.0.0.1:9042/something").send().await?;
+    assert_eq!(response.status(), reqwest::StatusCode::NOT_FOUND);
+
+    Ok(())
 }
