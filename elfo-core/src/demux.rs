6a90911981ad0e2581dccf48e22191eb16e47adb
Ilya Titkov <ilya@titkov.me>
2026-04-09 12:48:01 +0300

    perf(core): flatten demux filter chain into a vector (#188)

--- elfo-core/src/demux.rs
+++ elfo-core/src/demux.rs
@@ -7,30 +7,24 @@ use crate::{Addr, envelope::Envelope};
 const OPTIMAL_COUNT: usize = 5;
 type Addrs = SmallVec<[Addr; OPTIMAL_COUNT]>;
 
+type Filter = Arc<dyn Fn(&Envelope, &mut Addrs) + Send + Sync>;
+
 // Actually, it's a private type, `pub` is for `Destination` only.
 #[derive(Default, Clone)]
 pub struct Demux {
-    #[allow(clippy::type_complexity)]
-    filter: Option<Arc<dyn Fn(&Envelope, &mut Addrs) + Send + Sync>>,
+    filters: SmallVec<[Filter; OPTIMAL_COUNT]>,
 }
 
 impl Demux {
     pub(crate) fn append(&mut self, f: impl Fn(&Envelope, &mut Addrs) + Send + Sync + 'static) {
-        self.filter = Some(if let Some(prev) = self.filter.take() {
-            Arc::new(move |envelope, addrs| {
-                prev(envelope, addrs);
-                f(envelope, addrs);
-            })
-        } else {
-            Arc::new(f)
-        })
+        self.filters.push(Arc::new(f));
     }
 
     // TODO: return an iterator?
     pub(crate) fn filter(&self, envelope: &Envelope) -> Addrs {
         let mut addrs = Addrs::new();
-        if let Some(filter) = &self.filter {
-            (filter)(envelope, &mut addrs);
+        for f in &self.filters {
+            f(envelope, &mut addrs);
         }
         addrs
     }
